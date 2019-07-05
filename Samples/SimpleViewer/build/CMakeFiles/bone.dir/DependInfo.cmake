fa6bc66bb1929b44cc0bc49a40a1c4874704121c
choonyip <choonyip_8182@hotmail.com>
2019-07-05 10:34:16 +0900

    First commit

---
+++ Samples/SimpleViewer/build/CMakeFiles/bone.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/cy8182/repos/OpenNI-Linux-x64-2.3/Samples/SimpleViewer/Viewer.cpp" "/home/cy8182/repos/OpenNI-Linux-x64-2.3/Samples/SimpleViewer/build/CMakeFiles/bone.dir/Viewer.cpp.o"
+  "/home/cy8182/repos/OpenNI-Linux-x64-2.3/Samples/SimpleViewer/main.cpp" "/home/cy8182/repos/OpenNI-Linux-x64-2.3/Samples/SimpleViewer/build/CMakeFiles/bone.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/cy8182/repos/OpenNI-Linux-x64-2.3/Include"
+  "/home/cy8182/repos/NiTE-2.0.0/Include"
+  "../"
+  "/opt/ros/kinetic/include/opencv-3.3.1-dev"
+  "/opt/ros/kinetic/include/opencv-3.3.1-dev/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Samples/SimpleViewer/build/CMakeFiles/bone.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bone.dir/main.cpp.o"
+  "CMakeFiles/bone.dir/Viewer.cpp.o"
+  "bone.pdb"
+  "bone"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bone.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
